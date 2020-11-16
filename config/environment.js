2a251ce7ebb4acd1d091e19769eb18fee6e3fc84
Benga <benga.olasebikan@gmail.com>
2020-11-16 15:40:55 +0000

    atlas added

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 //! Consts
 const port = process.env.PORT || 8000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/clothes'
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://bengaboy:Gbenga90@kebb-clothes.tone1.mongodb.net/kebb-clothes?retryWrites=true&w=majority'
 const secret = process.env.SECRET || 'bengasmells'
 
 
